b00e918aaa3e582087c2ff0919f6b831d8f26d05
Sidaqdeep <behalsidaqdeepsingh@gmail.com>
2025-09-15 19:32:23 +0530

    making of rutes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+
 
 dotenv.config({
     path: "./.env"
@@ -8,7 +11,7 @@ dotenv.config({
 
 connectDB()
 .then(() => {
-  application.listen(process.env.PORT || 8000,() => {
+  app.listen(process.env.PORT || 8000,() => {
     console.log(`Server is running on port ${process.env.PORT}`);
   })
 })
